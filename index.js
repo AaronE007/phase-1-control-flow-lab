038cadd7d8e1207b369eda82593cede6a96f12a5
Aaron Elias <aaronelias007@gmail.com>
2021-12-06 21:43:28 -0500

    commit tree 3a6661b2e310f40ff52bf7115f3b8d62a9e21da1

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+let result 
+if (someNumber <= 400) {
+  return "This one is on me!"
+}
+  {let result
+  if (someNumber > 2500)
+  return "No can do."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+ {let result
+if (someNumber = 2001)
+return "I will gladly take your thirty bucks."}
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(place){
+  return place === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+}
+function switchOnCharmFromTip(tip){
+switch (tip) {
+    case 'generous' :
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+  default: return 'Bye.'; 
+  }
 }
